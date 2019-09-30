086a52305b38185ff02a321d4764b97b044f88c0
jmackay_doterra <jmackay@doterra.com>
2019-09-30 15:07:17 -0600

    Challenges 1-3 completed, challenge 4 is in progress

---
+++ challenge2/challenge2.py
@@ -0,0 +1,34 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Challenge2(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge2(self):
+        self.driver.get("https://www.copart.com")
+
+        # Finds search input and searches for 'exotics'
+        search = self.driver.find_element(By.ID , "input-search")
+        search.send_keys("exotics" + Keys.RETURN)
+
+        # Waits until the results table is visible
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"serverSideDataTable\"]//tbody")))
+
+        # Searches the results table for any and all 'PORSCHE'
+        makes = self.driver.find_elements_by_xpath("//*[@id=\"serverSideDataTable\"]//span[contains(text(),\"PORSCHE\")]")
+
+        # Asserts that at least one 'PORSCHE' was found in the results table
+        self.assertTrue(len(makes)>0,"Porsche was NOT found in the search results.")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ challenge3/challenge3.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Challenge3(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge3(self):
+        self.driver.get("https://www.copart.com")
+
+        # Waiting until the "Popular Items" section to load (sometimes the page loads slowly and the test fails)
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@ng-if=\"popularSearches\"]//a")))
+
+        # Searches for all of the Makes/Models in the "Popular Items" section
+        makes = self.driver.find_elements(By.XPATH, "//*[@ng-if=\"popularSearches\"]//a")
+
+        # Prints all of the texts & links
+        for make in makes:
+            print(make.text + " - " + make.get_attribute("href"))
+
+if __name__ == '__main__':
+    unittest.main()
