fd0e1f65397d9d3eab60ecb412843b6caaf3c0d8
jmackay_doterra <jmackay@doterra.com>
2019-10-10 12:14:02 -0600

    Adding in current progress with challenge 5

---
+++ challenge5/challenge5.py
@@ -0,0 +1,76 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Challenge5(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge2(self):
+        self.driver.maximize_window()
+        self.driver.get("https://www.copart.com")
+
+        # Finds search input and searches for 'exotics'
+        search = self.driver.find_element(By.ID , "input-search")
+        search.send_keys("porsche" + Keys.RETURN)
+
+        # Waits until the results table is visible
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"serverSideDataTable\"]//tbody")))
+
+        # Selects to show 100 items in the results
+        itemsToShow = self.driver.find_element(By.XPATH, "//*[@id=\"serverSideDataTable_length\"]//select")
+        itemsToShow.click()
+        option = self.driver.find_element(By.XPATH, "//*[@id=\"serverSideDataTable_length\"]//option[3]")
+        option.click()
+
+        # Waits until the results table has reloaded
+        WebDriverWait(self.driver, 30).until(
+            EC.invisibility_of_element_located((By.ID, "serverSideDataTable_processing")))
+
+        # Finds all models returned
+        models = self.driver.find_elements(By.XPATH, "//*[@id=\"serverSideDataTable\"]//span[@data-uname=\"lotsearchLotmodel\"]")
+
+        # Finds all unique models from list and adds them as a key value to a dictionary and updates the counts for each
+        uniqueModels = {}
+        for i in range(len(models)):
+            if (models[i].text in uniqueModels.keys()):
+                uniqueModels[models[i].text] += 1
+            else:
+                uniqueModels[models[i].text] = 1
+
+        print("Unique Models Found: ")
+        for i in uniqueModels:
+            print(i, "(", uniqueModels[i], ")")
+
+        # Finds all of the damages returned
+        damages = self.driver.find_elements(By.XPATH, "//*[@id=\"serverSideDataTable\"]//span[@data-uname=\"lotsearchLotdamagedescription\"]")
+
+        damageTypes = {
+            "REAR END": 0,
+            "FRONT END": 0,
+            "MINOR DENT/SCRATCHES": 0,
+            "UNDERCARRIAGE": 0,
+            "MISC": 0}
+
+        # Adds counts to the categories defined in the dictionary
+        for i in range(len(damages)):
+            if (damages[i].text in damageTypes.keys()):
+                damageTypes[damages[i].text] += 1
+            else:
+                damageTypes["MISC"] += 1
+
+        print("Damages: ")
+        for i in damageTypes:
+            print(i, "(", damageTypes[i], ")")
+
+
+if __name__ == '__main__':
+    unittest.main()
