b644da9f998a146ca6c57f35aebad0a8d96adeb3
jmackay_doterra <jmackay@doterra.com>
2019-10-11 09:14:33 -0600

    Adding current progress with challenge 6

---
+++ challenge6/challenge6.py
@@ -0,0 +1,60 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Challenge6(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_challenge6(self):
+        self.driver.maximize_window()
+        self.driver.get("https://www.copart.com")
+
+        search = self.driver.find_element(By.ID , "input-search")
+        search.send_keys("nissan" + Keys.RETURN)
+
+        # Waits until the model filter is visible
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"filters-collapse-1\"]//a[@data-uname=\"ModelFilter\"]")))
+
+        modelFilter = self.driver.find_element(By.XPATH, "//*[@id=\"filters-collapse-1\"]//a[@data-uname=\"ModelFilter\"]")
+        modelFilter.click()
+
+        modelFilterSearch = self.driver.find_element(By.XPATH, "//*[@id=\"collapseinside4\"]//input[@ng-model=\"filter.searchText\"]")
+        modelFilterSearch.send_keys("skyline")
+        # modelFilterSearch.send_keys("invalid_model")
+
+        vehicles = 0
+
+        try:
+            skylineCheckBox = self.driver.find_element(By.ID, "lot_model_descSKYLINE")
+            skylineCheckBox.click()
+
+            # Waits until the results table has reloaded
+            WebDriverWait(self.driver, 30).until(
+                EC.invisibility_of_element_located((By.ID, "serverSideDataTable_processing")))
+
+            # Finds all vehicles returned of that model
+            models = self.driver.find_elements(By.XPATH,
+                                                 "//*[@id=\"serverSideDataTable\"]//span[@data-uname=\"lotsearchLotmodel\"]")
+
+            vehicles = len(models)
+            print("Found", vehicles, "vehicle(s) of that model.")
+
+        except:
+            self.driver.save_screenshot("screenshot.png")
+            print("A screenshot has been saved as screenshot.png")
+
+
+        self.assertTrue(vehicles > 0, "No vehicles were found with that model filter.")
+
+
+if __name__ == '__main__':
+    unittest.main()
