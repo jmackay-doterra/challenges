91e21a40c72a0cdc128e042c878e9ead407b8873
jmackay_doterra <jmackay@doterra.com>
2019-10-21 10:23:03 -0600

    Updating challenge7 to use a 2D array as per requirements

--- challenge7/challenge7.py
+++ challenge7/challenge7.py
@@ -25,15 +25,30 @@ class Challenge7(unittest.TestCase):
         # Searches for all of the Makes/Models in the "Popular Items" section
         links = self.driver.find_elements(By.XPATH, "//*[@ng-if=\"popularSearches\"]//a")
 
-        # Checks that all URL's return a valid status code
-        for link in range(len(links)):
-            linkText = links[link].text
-            linkURL = links[link].get_attribute("href")
+        # Stores link text and links in a 2D array
+        linksArray = []
+        for x in range(len(links)):
+            linksArray.append([])
+            for y in range(1):
+                linksArray[x].append(links[x].text)
+                linksArray[x].append(links[x].get_attribute("href"))
+
+            # Checks that the URL returns a valid status code
             try:
-                r = requests.head(linkURL)
-                print(linkText, ":", linkURL, ": Status Code", r.status_code, "(PASSED)")
-            except requests.ConnectionError:
-                print(linkText, ":", linkURL, ": FAILED to connect")
+                r = requests.head(linksArray[x][1])
+                print(linksArray[x][0], linksArray[x][1], ": Status Code", r.status_code, "(PASSED)")
+            except:
+                print(linksArray[x][0], linksArray[x][1], "FAILED to connect")
+
+        # # OLD CODE USED BEFORE SEEING THAT A 2D ARRAY WAS IN THE REQUIREMENTS
+        # for link in range(len(links)):
+        #     linkText = links[link].text
+        #     linkURL = links[link].get_attribute("href")
+        #     try:
+        #         r = requests.head(linkURL)
+        #         print(linkText, ":", linkURL, ": Status Code", r.status_code, "(PASSED)")
+        #     except requests.ConnectionError:
+        #         print(linkText, ":", linkURL, ": FAILED to connect")
 
 if __name__ == '__main__':
     unittest.main()
