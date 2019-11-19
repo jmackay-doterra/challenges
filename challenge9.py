2781b854b37af47abbc3c10f9c332c6970420d1a
jmackay_doterra <jmackay@doterra.com>
2019-11-19 08:56:25 -0700

    Next challenge upload

--- challenge9.py
+++ challenge9.py
@@ -1,20 +1,31 @@
 import unittest
 import requests
+import random
 from _common.api import Api
 
-class Challenge8(unittest.TestCase):
+class Challenge9(unittest.TestCase):
 
-    def test_challenge8(self):
+    def test_challenge9(self):
         url = "https://www.copart.com/public/lots/search"
         queries = ["jeep", "jeep wrangler", "cherokee", "jeep cherokee", "lamborghini", "ferrari", "mustang",
                    "corvette", "porsche", "chevrolet silverado"]
         headers = Api.getHeaders(self)
+        expectedTypes = Api.getExpectedTypes(self)
 
         for query in range(len(queries)):
             payload = Api.getPayload(self, queries[query])
             response = requests.post(url, data=payload, headers=headers)
             totalElements = str(response.json()['data']['results']['totalElements'])
             print(totalElements + " results for '" + queries[query] + "'")
+
+            print("Validating types of one random object:")
+            randomObject = random.randint(1, int(len(response.json())))
+            for i in response.json()['data']['results']['content'][randomObject]:
+                data = response.json()['data']['results']['content'][randomObject][i]
+                print(" ", i, ':', data, type(data))
+                assert(type(data)==expectedTypes[i])
+
+            print("--------------------------------------------------")
 
 if __name__ == '__main__':
     unittest.main()
